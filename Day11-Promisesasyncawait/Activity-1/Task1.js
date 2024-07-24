9f22266cbebef2eb3790f661cd004001079b7044
its-me-santey <lazypy12@gmail.com>
2024-07-25 00:23:58 +0545

    day 11 task has started

---
+++ Day11-Promisesasyncawait/Activity-1/Task1.js
@@ -0,0 +1,13 @@
+//create a promise that resolves with a message afte a 2-second timeout and log the message to the  console.
+
+
+const myPromise = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('this is a resolved message after 2 seconds!!!'),2000
+    })
+})
+
+
+myPromise.then((message)=>{
+    console.log(message)
+})
